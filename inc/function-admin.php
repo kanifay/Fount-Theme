6da079b1e8eb1c62c0fb7dcd46d5d52912393aee
Kani frances <kani@mediauganda.com>
2022-06-14 14:07:32 +0300

    https://github.com/kanifrances/Fount-Theme-Development.git

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -55,83 +55,53 @@ function fount_add_admin_page(){
 
       //Activate custom settings
       //admin_init is triggered before any other hook when a user accesses the admin area.
-      add_action(  'admin_init',
-                    'fount_custom'
-                    );
-
+      add_action( 'admin_init','fount_custom');
 }
 
 // trigger function using function 
 //admin_menu action is used to add extra submenus and menu options to the admin panel's menu structure
-add_action(   'admin_menu' , 
-              'fount_add_admin_page');
+add_action(   'admin_menu' , 'fount_add_admin_page');
 
 // register_settings registers a setting and its data.
 function fount_custom() {
 
-    register_setting( 'fount-settings-group',
-                      'profile_picture'
-                    );  
+      register_setting( 'fount-settings-group',   'profile_picture' );  
+      register_setting( 'fount-settings-group',   'user_description' );    
+      register_setting( 'fount-settings-group',   'first_name' );
+      register_setting( 'fount-settings-group',   'last_name' );
+      register_setting( 'fount-settings-group',   'twitter_handler', 'fount_sanitize_twitter_handler' );   
+      register_setting( 'fount-settings-group',   'facebook_handler' );  
+      register_setting( 'fount-settings-group',   'instagram_handler' );  
 
-    register_setting( 'fount-settings-group',
-                      'user_description'
-                    );    
-  
-    register_setting(   'fount-settings-group',
-                        'first_name' 
-                    );
-
-    register_setting( 'fount-settings-group',
-                      'last_name'
-                    );
-
-    register_setting( 'fount-settings-group',
-                      'twitter_handler',
-                      'fount_sanitize_twitter_handler'
-                    );   
-                    
-    register_setting( 'fount-settings-group',
-                      'facebook_handler'
-                    );  
-
-    register_setting( 'fount-settings-group',
-                      'instagram_handler'
-                    );  
     // define new settings sections for an admin page or sub menu page
-    add_settings_section('fount-sidebar-options',
-                          'Side Sidebar Options',
-                          'fount_sidebar',
-                          'fay_fount'
-                          ); 
-          //============================================================
-              add_settings_field(  'sidebar-profile',
+    add_settings_section('fount-sidebar-options', 'Side Sidebar Options', 'fount_sidebar','fay_fount' ); 
+          //gives us the ability to generate custom html input Adds a new field to a section of a settings page.
+              add_settings_field( 'sidebar-profile',
                                   'Profile Picture',
                                   'fount_profile',
                                   'fay_fount',
                                   'fount-sidebar-options'
                                 );
 
-              add_settings_field(  'sidebar-description',
-                                    'User Description', 
-                                    'fount_description',
-                                    'fay_fount',
-                                    'fount-sidebar-options'
+              add_settings_field( 'sidebar-description',
+                                  'User Description', 
+                                  'fount_description',
+                                  'fay_fount',
+                                  'fount-sidebar-options'
                                 );
 
-              
-                add_settings_field( 'sidebar-name',
-                                    'First Name', 
-                                    'fount_sidebar_name',
-                                    'fay_fount',
-                                    'fount-sidebar-options'
-                                    
+              add_settings_field( 'sidebar-name',
+                                  'First Name', 
+                                  'fount_sidebar_name',
+                                  'fay_fount',
+                                  'fount-sidebar-options'
                                 ); 
 
-                add_settings_field( 'sidebar-lname',
-                                    'Last Name', 
-                                    'fount_lname',
-                                    'fay_fount',
-                                    'fount-sidebar-options'
+              add_settings_field( 'sidebar-lname',
+                                  'Last Name', 
+                                  'fount_lname',
+                                  'fay_fount',
+                                  'fount-sidebar-options'
                                 
                                 ); 
 
@@ -181,128 +151,115 @@ function fount_custom() {
                                       'fay_fount_theme ',
                                       'fount-theme-options'
                                     );
+} 
 
-
-  } 
-
-
-  //Post Formats Callback Function
-  function fount_post_formats_callback($input){
+//Post Formats Callback Function
+function fount_post_formats_callback($input){
     return $input;
-  }
+}
 
-  function fount_theme_options(){
-    echo 'Activate and Deactivate specific Theme Support Options';
-  }
+function fount_theme_options(){
+  echo 'Activate and Deactivate specific Theme Support Options';
+}
 
-  //loops formats 9times
-  function fount_post_formats(){
-    $options = get_option( 'post_formats' );
-    $formats = array('aside' , 'gallery' , 'links', 'image', 'quote' , 'status' , 'video' , 'audio' ,'chat' ); 
-    $output  = '';
-      foreach( $formats as $format){
-        $checked = ( @$options[$format] == 1 ? 'checked' : '');
-        $output .= '<label> <input type = "checkbox" id = "'.$format.'"   name = " '.$format.' "  value = "1" > '.$checked.'</label> <br>';
-  }
-  echo $output;
+//loops formats 9times
+function fount_post_formats(){
+  $options = get_option( 'post_formats' );
+  $formats = array('aside' , 'gallery' , 'links', 'image', 'quote' , 'status' , 'video' , 'audio' ,'chat' ); 
+  $output  = '';
+    foreach( $formats as $format){
+      $checked = ( @$options[$format] == 1 ? 'checked' : '');
+      $output .= '<label> <input type = "checkbox" id = "'.$format.'"   name = " '.$format.' "  value = "1" > '.$checked.'</label> <br>';
+}
+echo $output;
 
-  }
+}
 
-  function fount_theme_create_page(){
-      //generates our admin page
-      //The require_once keyword is used to embed PHP code from another file
-        require_once( get_template_directory() . '/inc/templates/fount-admin.php');
-  } 
+function fount_theme_create_page(){
+    //generates our admin page
+    //The require_once keyword is used to embed PHP code from another file
+      require_once( get_template_directory() . '/inc/templates/fount-admin.php');
+} 
 
-  //template submenu functions
-  function fount_theme_support_page(){
-    require_once(get_template_directory() . '/inc/templates/fount-theme-support.php');
+//template submenu functions
+function fount_theme_support_page(){
+  require_once(get_template_directory() . '/inc/templates/fount-theme-support.php');
 
-  }
+}
 
-  function fount_theme_settings_page(){
+function fount_theme_settings_page(){
+  echo '<h5> Fount Fount Custom CSS </h5>' ;
+  echo '<h5> It is working </h5>' ;
+
+    //generates our admin page
+      
+} 
+
+function fount_theme_customs_page(){
+  require_once(get_template_directory() . '/inc/templates/movies-slider.php');
+  //generates our admin page       
+} 
+
+
+function fount_sidebar(){
+  //generates our admin page
     echo '<h5> Fount Fount Custom CSS </h5>' ;
     echo '<h5> It is working </h5>' ;
+    
+} 
 
-      //generates our admin page
-        
-  } 
+function fount_profile(){
+  $profile = esc_attr( get_option(' profile_picture ') );
+      echo '<input type = "button" value ="Upload Profile Picture" id = "upload-button" >
+            <input type = "hidden"  id = "profile-picture" name = "profile_picture"  value = "'.$profile.'"  /> ';  
+      echo "</br>"; 
+}
 
-  function fount_theme_customs_page(){
-    require_once(get_template_directory() . '/inc/templates/movies-slider.php');
-    //generates our admin page       
-  } 
+function fount_description(){
+  $description = esc_attr( get_option(' user_description') );
+      echo '<input type = "text" name = "user_description"  value = "'.$description.'" placeholder = "User Description" /> 
+            <p class = "user-description"> Tell us about you!</p>';
+}
 
+function fount_sidebar_name(){
+  $firstName = esc_attr( get_option('first_name') );
+        echo '<input type = "text" name = "first_name" value = "'.$firstName.'" placeholder = "First Name" /> '; 
+}
 
-  function fount_sidebar(){
-    //generates our admin page
-      echo '<h5> Fount Fount Custom CSS </h5>' ;
-      echo '<h5> It is working </h5>' ;
-      
-  } 
+function fount_lname(){
+  $lastName = esc_attr( get_option('last_name') );
+        echo '<input type = "text" name = "last_name" value = "'.$lastName.'" placeholder = "Last Name" /> '; 
+        echo "</br>";
+}
 
-  function fount_profile(){
-    $profile = esc_attr( get_option(' profile_picture ') );
-        echo '<input type = "button" value ="Upload Profile Picture" id = "upload-button" >
-        <input type = "hidden"  id = "profile-picture" name = "profile_picture"  value = "'.$profile.'"  /> ';
-      
-        echo "</br>"; 
-  }
+function fount_twitter() {
+  $twitterHandler = esc_attr( get_option(' twitter_handler') );
+          echo '<input type = "text" name = "twitter_handler"  value = "'.$twitterHandler.'" placeholder = "Twitter Handler" /> 
+                <p class = "description"> @ symbol has been removed</p>';
+}
 
+function fount_facebook(){
+  $facebook = esc_attr( get_option(' facebook_handler') );
+          echo '<input type = "text" name = "facebook_handler"  value = "'.$facebook.'" placeholder = "Facebook Handler" /> '; 
+          echo "</br>";
+}
 
-  function fount_description(){
-    $description = esc_attr( get_option(' user_description') );
-        echo '<input type = "text" name = "user_description"  value = "'.$description.'" placeholder = "User Description" /> 
-              <p class = "user-description"> Tell us about you!</p>';
-        echo "</br>"; 
-  }
-
-
-  function fount_sidebar_name(){
-    $firstName = esc_attr( get_option('first_name') );
-          echo '<input type = "text" name = "first_name" value = "'.$firstName.'" placeholder = "First Name" /> '; 
+function fount_instagram(){
+  $instagramHandler = esc_attr( get_option(' instagram_handler') );
+          echo '<input type = "text" name = "instagram_handler"  value = "'.$instagramHandler.'" placeholder = "instagram Handler" /> '; 
           echo "</br>";
           echo "</br>";
-  }
+}
 
-  function fount_lname(){
-    $lastName = esc_attr( get_option('last_name') );
-          echo '<input type = "text" name = "last_name" value = "'.$lastName.'" placeholder = "Last Name" /> '; 
-          echo "</br>";
-          echo "</br>";
-  }
+//sanitize twitter settings---- checks for invalid user typos
+function fount_sanitize_twitter_handler( $input ){
+          $output = sanitize_text_field($input);
+          $output = str_replace('@', '' , $output);
+          return $output;
 
-
-  function fount_twitter() {
-    $twitterHandler = esc_attr( get_option(' twitter_handler') );
-            echo '<input type = "text" name = "twitter_handler"  value = "'.$twitterHandler.'" placeholder = "Twitter Handler" /> 
-            <p class = "description"> @ symbol has been removed</p>';
-            echo "</br>";
-  }
-
-  function fount_facebook(){
-    $facebook = esc_attr( get_option(' facebook_handler') );
-            echo '<input type = "text" name = "facebook_handler"  value = "'.$facebook.'" placeholder = "Facebook Handler" /> '; 
-            echo "</br>";
-            echo "</br>";
-  }
-
-  function fount_instagram(){
-    $instagramHandler = esc_attr( get_option(' instagram_handler') );
-            echo '<input type = "text" name = "instagram_handler"  value = "'.$instagramHandler.'" placeholder = "instagram Handler" /> '; 
-            echo "</br>";
-            echo "</br>";
-  }
-
-  //sanitize twitter settings---- checks for invalid user typos
-  function fount_sanitize_twitter_handler( $input ){
-            $output = sanitize_text_field($input);
-            $output = str_replace('@', '' , $output);
-            return $output;
-
-  }
+}
 
 
 
-  ?>
+?>
 
--- inc/templates/fount-admin.php
+++ inc/templates/fount-admin.php
@@ -14,8 +14,10 @@
         <h1> Fount sidebar Options</h1>
         <!-- <h4 class = "title-one">Manage Options</h4>
         <p>This is where all the administrative work goes!</p> -->
-
-        <?php settings_errors(); ?>
+        
+<?php 
+//Registers a settings error to be displayed to the user.
+settings_errors(); ?>
 <?php
         $profile                = esc_attr( get_option(' profile_picture') );
         $description            = esc_attr( get_option(' user_description') );
@@ -26,43 +28,29 @@
         $instagramHandler       = esc_attr( get_option(' instagram_handler') );
 
 ?>
-
         <!--   -->
-         <div class = "fount-sidebar-preview"> 
-            <div class = "fount-sidebar">
-
+<div class = "fount-sidebar-preview"> 
+        <div class = "fount-sidebar">
                 <div class = "image-container">
-                    <div id = "profile-picture-preview" class = "profile-picture"
+                        <div id = "profile-picture-preview" class = "profile-picture"
                         style = "background-image: url (< ?php print $profile; ?> );">
-                    </div>   
+                        </div>   
                 </div>
-
-                <h1 class = "fount-firstname">   <?php print $firstName;   ?></h1>
-                <h1 class = "fount-lastname">    <?php print $lastName;    ?></h1>
-                <h2 class = "fount-description"> <?php print $description; ?></h2>
-                
+                        <h1 class = "fount-firstname">   <?php print $firstName;   ?></h1>
+                        <h1 class = "fount-lastname">    <?php print $lastName;    ?></h1>
+                        <h2 class = "fount-description"> <?php print $description; ?></h2>
                 <div class = "icons-wrapper">
 
-
                 </div>
-            </div>     
-         </div>
-
-
+        </div>     
+</div>
         <form method = "post" action = "options.php" class = "fount-general-form">
-            <!-- Settings_fields Output nonce, action, and option_page fields for a settings page.-->
-           <?php settings_fields('fount-settings-group'); ?>
-
-           <!-- Prints out all settings sections added to a particular settings page -->
-           <?php do_settings_sections('fay_fount') ; ?>
-
-           <?php submit_button(); ?>
-        
+                <!-- Settings_fields Output nonce, action, and option_page fields for a settings page.-->
+                <?php settings_fields('fount-settings-group'); ?>
+                <!-- Prints out all settings sections added to a particular settings page -->
+                <?php do_settings_sections('fay_fount') ; ?>
+                <?php submit_button(); ?>
         </form>
-
-        
-
-
 </body>
 </html>
 
